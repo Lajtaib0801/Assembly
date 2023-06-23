1773877e53067beaccf67f24b9b27df0e84cb487
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2023-06-23 20:19:28 +0200

    Update cpuid2.s

--- gas/04_cpuid/cpuid2.s
+++ gas/04_cpuid/cpuid2.s
@@ -1,5 +1,6 @@
 #cpuid with printf (C library calls)
 #command to make it on 64 bit linux based systems: as -o cpuid2.o cpuid2.s --32 && ld -dynamic-linker /lib/ld-linux.so.2 -o cpuid2 -lc cpuid2.o -m elf_i386
+#OR you can use simply: gcc -o cpuid2 cpuid2.s -no-pie -m32 BUT in this case you have to use 'main' instead of '_start' entry point
 .section .data
 output:
 	.asciz "The processor Vendor ID is '%s'\n"	#asciz adds \0 to the end of the string, ascii doesn't
