fac9f13ad04313419baf7d463658e5250d484eac
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2023-06-27 17:46:31 +0200

    Update swaptest.s

--- gas/05_sizetest/swaptest.s
+++ gas/05_sizetest/swaptest.s
@@ -2,8 +2,8 @@
 #command to make it on 64 bit linux based systems: as -o swaptest.o swaptest.s && ld -o swaptest swaptest.o
 #OR you can use simply: gcc -o swaptest swaptest.s BUT in this case you have to use 'main' instead of '_start' entry point
 .section .text
-.globl main
-main:
+.globl _start
+_start:
     movl $0x12345678, %ebx      #move 0x12345678 into EBX
     bswap %ebx                  #swap the BYTES order => 0x12345678 --> 0x78563412
     movl $1, %eax
