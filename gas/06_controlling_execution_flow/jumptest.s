62fbd5a58e1963e1b439d2e8df3eed6ea2659b33
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2023-06-30 15:31:51 +0200

    Create jumptest.s

---
+++ gas/06_controlling_execution_flow/jumptest.s
@@ -0,0 +1,11 @@
+#jumptest.s - An example of jmp instruction
+.section .text
+.globl _start
+_start:
+    movl $1, %eax
+    jmp overhere
+    movl $10, %ebx
+    int $0x80
+overhere:
+    movl $20, %ebx
+    int $0x80
