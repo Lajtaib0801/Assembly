268622c320fb93c0df42962b34b2468514be5918
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2023-07-12 14:48:33 +0200

    Add files via upload

---
+++ gas/07_using_numbers/ssetest.s
@@ -0,0 +1,15 @@
+#ssetest.s - An example of using 128-bit SSE registers
+.section .data
+values1:
+	.int 1, -1, 0, 135246
+values2:
+	.quad 1, -1				#quad is 64bit, so this is 128bit data
+.section .text
+.globl _start
+_start:
+	movdqu values1, %xmm0	#xmm0 and xmm1 are 128bit registers
+	movdqu values2, %xmm1
+
+	movl $1, %eax
+	movl $0, %ebx
+	int $0x80
