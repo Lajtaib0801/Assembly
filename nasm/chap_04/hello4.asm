8f8953d9dfb748906ce522babc9a88e76ec4f1ee
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2023-07-20 13:05:46 +0200

    Update hello4.asm

--- nasm/chap_04/hello4.asm
+++ nasm/chap_04/hello4.asm
@@ -2,7 +2,7 @@
 extern printf									;tell the compiler that we will use an external function (printf)
 section .data
 	msg		db	"Hello, World!",0				;we need zero-terminated strings, if we use printf
-	fmtstr	db	"This is our string: %s",10,0	;formatstring, like C
+	fmtstr	db	"This is our string: %s",10,0	;formatstring, like C 
 
 section .text
 global main
