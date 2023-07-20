18001fb18b63a31145b3b1c083648f21e2ecdfff
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2023-07-20 13:06:04 +0200

    Update hello4.asm

--- nasm/chap_04/hello4.asm
+++ nasm/chap_04/hello4.asm
@@ -8,13 +8,13 @@ section .text
 global main
 main:
 	push	rbp
-	mov		rbp, rsp
-	mov		rdi, fmtstr
-	mov		rsi, msg
-	mov		rax, 0
+	mov	rbp, rsp
+	mov	rdi, fmtstr
+	mov	rsi, msg
+	mov	rax, 0
 	call printf
-	mov		rsp, rbp
-	pop		rbp
-	mov		rax, 60
-	mov		rdi, 0
+	mov	rsp, rbp
+	pop	rbp
+	mov	rax, 60
+	mov	rdi, 0
 	syscall
