1672651d4ce2cc38c9b0767051d791e28b503fa1
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2023-07-21 20:37:01 +0200

    Add files via upload

---
+++ nasm/chap_07/betterloop.asm
@@ -0,0 +1,27 @@
+;betterloop.asm
+
+extern printf
+section .data
+    number  dq  1000000000
+    fmt     db  "The sum from 0 to %ld is %ld",10,0
+
+section .text
+global main
+main:
+    push    rbp
+    mov     rbp, rsp
+    mov     rcx, [number]   ;initialize rcx with number
+    mov     rax, 0
+    
+bloop:
+    add     rax, rcx        ;add rcx to sum
+    loop    bloop           ;loop while decreasing rcx with 1
+                            ;until rcx = 0
+    mov     rdi, fmt
+    mov     rsi, [number]
+    mov     rdx, rax
+    mov     rax, 0          ;no floating point
+    call printf
+    mov     rbp, rsp
+    pop     rbp
+    ret
